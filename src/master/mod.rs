b54ed53e375c5f64dbfddef87de5a3fb0a24b333
Douman <douman@gmx.se>
2019-07-28 07:48:26 +0200

    Add x11 clipboard support

--- src/master/mod.rs
+++ src/master/mod.rs
@@ -1,2 +1,5 @@
 #[cfg(windows)]
 mod win32;
+
+#[cfg(all(unix, not(any(target_os="macos", target_os="android", target_os="emscripten"))))]
+mod x11;
---
+++ src/master/x11.rs
@@ -0,0 +1,34 @@
+use std::io;
+use crate::{ClipboardHandler, CallbackResult, Master};
+
+impl<H: ClipboardHandler> Master<H> {
+    ///Starts Master by creating dummy window and listening clipboard update messages.
+    pub fn run(&mut self) -> io::Result<()> {
+        let mut result = Ok(());
+        let clipboard = x11_clipboard::Clipboard::new().unwrap();
+
+        loop {
+            match clipboard.load_wait(clipboard.getter.atoms.primary, x11_clipboard::xcb::xproto::ATOM_ANY, clipboard.getter.atoms.property) {
+                Ok(_) => match self.handler.on_clipboard_change() {
+                    CallbackResult::Next => (),
+                    CallbackResult::Stop => break,
+                    CallbackResult::StopWithError(error) => {
+                        result = Err(error);
+                        break;
+                    }
+
+                },
+                Err(error) => match self.handler.on_clipboard_error(io::Error::new(io::ErrorKind::Other, error)) {
+                    CallbackResult::Next => (),
+                    CallbackResult::Stop => break,
+                    CallbackResult::StopWithError(error) => {
+                        result = Err(error);
+                        break;
+                    }
+                }
+            }
+        }
+
+        result
+    }
+}
