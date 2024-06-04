1cf636770e49a4d63183f7594ff564f63bc6ee00
Douman <douman@gmx.se>
2024-06-04 23:40:53 +0900

    Enable graceful shutdown on linux (#13)
    * Enable graceful shutdown on linux
    
    * Re-implement raw x11 poll for event

--- tests/shutdown.rs
+++ tests/shutdown.rs
@@ -1,26 +1,29 @@
+use std::time;
 use clipboard_master::{Master, ClipboardHandler, CallbackResult};
 
 pub struct Handler;
 
 impl ClipboardHandler for Handler {
     fn on_clipboard_change(&mut self) -> CallbackResult {
+        println!("CHANGE");
         CallbackResult::Next
     }
 }
 
-//TODO: Make shutdown work on Linux
-//This is currently difficult due to buggy x11-clipboard lib
-#[cfg(not(target_arch = "linux"))]
 #[test]
 fn should_shutdown_successfully() {
+    const TIMEOUT: time::Duration = time::Duration::from_secs(5);
     let mut master = Master::new(Handler).expect("To create master");
     let shutdown = master.shutdown_channel();
     std::thread::spawn(move || {
-        std::thread::sleep(core::time::Duration::from_secs(5));
+        std::thread::sleep(TIMEOUT);
         println!("signal");
         shutdown.signal();
     });
 
     println!("RUN");
+    let now = time::Instant::now();
     master.run().expect("to finish");
+    assert!(now.elapsed() >= (TIMEOUT - time::Duration::from_millis(500)));
+    assert!(now.elapsed() <= (TIMEOUT + time::Duration::from_millis(500)));
 }
